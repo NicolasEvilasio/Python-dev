485375240c352cf2f69fedb3859ab32c71b21d9c
nicolas evilasio <nicolas_evilasio@hotmail.com>
2024-03-05 21:41:01 -0300

    polimorfismo do método abastecer

--- 1 - POO/carro.py
+++ 1 - POO/carro.py
@@ -6,4 +6,11 @@ class Carro(veiculo.Veiculo):
     def __init__(self, cor, tipo_combustivel, potencia, qtd_portas):
         super().__init__(cor, tipo_combustivel, potencia)
         self.qtd_portas = qtd_portas
+        self._libras = 30
 
+    def abastecer(self, qtd_combustivel):
+        print('O método foi chamado a partir da classe carro')
+        if self.qtd_combustivel >= 30:
+            print('O tanque do carro está cheio')
+        else:
+            super().abastecer(qtd_combustivel)
--- 1 - POO/moto.py
+++ 1 - POO/moto.py
@@ -6,4 +6,11 @@ class Moto(veiculo.Veiculo):
     def __init__(self, cor, tipo_combustivel, potencia, qtd_cilindradas):
         super().__init__(cor, tipo_combustivel, potencia)
         self.qtd_cilindradas = qtd_cilindradas
+        self._libras = 20
 
+    def abastecer(self, qtd_combustivel):
+        print('O método foi chamado a partir da classe moto')
+        if self.qtd_combustivel >= 15: # usando o getter qtd_combustivel definido na super classe
+            print('O tanque da moto está cheio')
+        else:
+            super().abastecer(qtd_combustivel) # usando o método definido na super classe
