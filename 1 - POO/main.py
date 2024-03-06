e63b3e7ee999d50ec9f66cf1fcc18c79e1b752d5
nicolas evilasio <nicolas_evilasio@hotmail.com>
2024-03-05 21:25:43 -0300

    criação de properties e alteração da visibilidade dos atributos da classe Veiculo para privado

--- 1 - POO/main.py
+++ 1 - POO/main.py
@@ -4,6 +4,10 @@ import moto
 uno_vermelho = carro.Carro('vermelho', 'gasolina', 90, 4)
 uno_vermelho.ligar()
 
+print(uno_vermelho.cor)
+uno_vermelho.cor = 'azul'
+print(uno_vermelho.cor)
+
 print(f'Combustível: {uno_vermelho.qtd_combustivel}')
 uno_vermelho.abastecer(10)
 print(f'Combustível: {uno_vermelho.qtd_combustivel}')
@@ -18,5 +22,3 @@ uno_vermelho.desligar()
 
 moto_vermelha = moto.Moto('vermelho', 'gasolina', 90, 110)
 moto_vermelha.ligar()
-
-
--- 1 - POO/veiculo.py
+++ 1 - POO/veiculo.py
@@ -2,35 +2,90 @@ class Veiculo:
     """Essa é a classe Veiculo. Essa classe é utilizada para instanciar novos veículos."""
 
     def __init__(self, cor, tipo_combustivel, potencia):
-        self.cor = cor
-        self.tipo_combustivel = tipo_combustivel
-        self.potencia = potencia
-        self.qtd_combustivel = 0
-        self.is_ligado = False
-        self.velocidade = 0
+        self.__cor = cor
+        self.__tipo_combustivel = tipo_combustivel
+        self.__potencia = potencia
+        self.__qtd_combustivel = 0
+        self.__is_ligado = False
+        self.__velocidade = 0
 
     def __del__(self):
         print('O objeto foi removido da memória.')
 
+    # Definiçao dos métodos da classe
     def abastecer(self, qtd_combustivel):
-        self.qtd_combustivel += qtd_combustivel
+        self.__qtd_combustivel += qtd_combustivel
 
     def ligar(self):
-        if self.is_ligado:
+        if self.__is_ligado:
             print('O veículo já está ligado!')
         else:
-            self.is_ligado = True
+            self.__is_ligado = True
             print('O veículo foi ligado!')
 
     def desligar(self):
-        if not self.is_ligado:
+        if not self.__is_ligado:
             print('O veículo já está desligado!')
         else:
-            self.is_ligado = False
+            self.__is_ligado = False
             print('O veículo foi desligado!')
 
     def acelerar(self, velocidade=10):
-        if self.is_ligado:
-            self.velocidade += velocidade
+        if self.__is_ligado:
+            self.__velocidade += velocidade
         else:
             print('O veículo está desligado!')
+
+    # Getters e Setters
+    @property
+    def cor(self):
+        return self.__cor
+
+    @property
+    def tipo_combustivel(self):
+        return self.__tipo_combustivel
+
+    @property
+    def potencia(self):
+        return self.__potencia
+
+    @property
+    def qtd_combustivel(self):
+        return self.__qtd_combustivel
+
+    @property
+    def is_ligado(self):
+        return self.__is_ligado
+
+    @property
+    def velocidade(self):
+        return self.__velocidade
+
+    @cor.setter
+    def cor(self, cor):
+        self.__cor = cor
+
+    @tipo_combustivel.setter
+    def tipo_combustivel(self, tipo_combustivel):
+        self.__tipo_combustivel = tipo_combustivel
+
+    @potencia.setter
+    def potencia(self, potencia):
+        self.__potencia = potencia
+
+
+# Testes rápidos
+if __name__ == '__main__':
+    uno = Veiculo('Vermelho', 'Gasolina', 1.0)
+
+    print(uno.cor)
+    uno.cor = 'Azul'
+    print(uno.cor)
+
+    print(uno.potencia)
+    uno.potencia = 2.0
+    print(uno.potencia)
+
+    print(uno.tipo_combustivel)
+    uno.tipo_combustivel = 'Etanol'
+    print(uno.tipo_combustivel)
