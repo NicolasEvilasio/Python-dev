256ea019f6adc6ba341766e054cb5ba735cae218
nicolas evilasio <nicolas_evilasio@hotmail.com>
2024-03-15 00:26:19 -0300

    feat: inclusão de docstring na função selection_sort e ajuste na estrutura da iteração

--- 3 - Algoritmos/selection_sort.py
+++ 3 - Algoritmos/selection_sort.py
@@ -1,9 +1,22 @@
-from gerar_lista import gerar_lista
-
-numeros = gerar_lista()
-
-
 def selection_sort(lista: list) -> list:
+    """
+    Ordena uma lista de elementos usando o algoritmo Selection Sort.
+
+    O Selection Sort é um algoritmo de ordenação que divide a lista em duas partes:
+    uma parte ordenada e outra parte desordenada. Ele procura o menor elemento
+    na parte desordenada e o move para a parte ordenada. Esse processo é repetido
+    até que todos os elementos estejam na parte ordenada.
+
+    Args:
+        lista (list): A lista de elementos a ser ordenada.
+
+    Returns:
+        list: A lista ordenada.
+
+    Examples:
+        >>> selection_sort([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])
+        [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
+    """
     tamanho_lista = len(lista)
 
     if tamanho_lista <= 0:
@@ -12,12 +25,20 @@ def selection_sort(lista: list) -> list:
 
     else:
         for i in range(tamanho_lista):
+            print(lista)
             menor_indice = i
+
             for j in range(i + 1, tamanho_lista):
-                if numeros[j] < numeros[i]:
-                    numeros[i], numeros[j] = numeros[j], numeros[i]
+                if lista[j] < lista[i]:
+                    menor_indice = j
 
-    return numeros
+            lista[i], lista[menor_indice] = lista[menor_indice], lista[i]
+
+    return lista
 
 
-print(selection_sort(numeros))
+if __name__ == '__main__':
+    from gerar_lista import gerar_lista
+
+    numeros = gerar_lista()
+    print(selection_sort(numeros))
