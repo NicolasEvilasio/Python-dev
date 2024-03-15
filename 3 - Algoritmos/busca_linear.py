dcbdc8d5d8f63728145605f829d9e97d7f1fc559
nicolas evilasio <nicolas_evilasio@hotmail.com>
2024-03-15 00:16:55 -0300

    feat: alteração do paradigma para funcional e inclusão da docstring da função

--- 3 - Algoritmos/busca_linear.py
+++ 3 - Algoritmos/busca_linear.py
@@ -1,19 +1,40 @@
-from gerar_lista import gerar_lista
+def busca_linear(valor_buscado: int, lista: list) -> int:
+    """
+    Realiza uma busca linear por um valor em uma lista e retorna a posição onde o valor foi encontrado.
 
-numeros = gerar_lista()
+    A busca linear é um algoritmo simples que percorre toda a lista sequencialmente, comparando cada elemento
+    com o valor buscado até que o valor seja encontrado ou até que toda a lista seja percorrida.
 
-# BUSCA LINEAR
-numero_pesquisar = int(input('Digite o valor a ser pesquisado no vetor: '))
-posicao_resultado = -1
+    Args:
+        valor_buscado (int): O valor que está sendo buscado na lista.
+        lista (list): A lista na qual o valor está sendo buscado.
 
-for i in range(len(numeros)):
-    if numeros[i] == numero_pesquisar:
-        posicao_resultado = i
-        break
+    Returns:
+        int: A posição do valor buscado na lista. Retorna -1 se o valor não for encontrado.
 
-if posicao_resultado < 0:
-    print('O elemento não foi encontrado no vetor')
-else:
-    print(f'O elemento {numero_pesquisar} foi encontrado na posição {posicao_resultado}')
+    Raises:
+        AttributeError: Se o valor buscado não for encontrado na lista.
+    """
+    posicao_resultado = -1
 
-# FIM BUSCA LINEAR
+    for i in range(len(lista)):
+        if numeros[i] == valor_buscado:
+            posicao_resultado = i
+            break
+
+    if posicao_resultado < 0:
+        print('O elemento não foi encontrado no vetor')
+        raise AttributeError
+
+    else:
+        return posicao_resultado
+
+
+if __name__ == '__main__':
+    from gerar_lista import gerar_lista
+
+    numeros = gerar_lista()
+    numero_pesquisar = int(input('Digite o valor a ser pesquisado no vetor: '))
+    posicao = busca_linear(numero_pesquisar, numeros)
+
+    print(f'O elemento {numero_pesquisar} foi encontrado na posição {posicao}')
