68180a9c738233d84cd8eac63cc1c315823aded6
nicolas evilasio <nicolas_evilasio@hotmail.com>
2024-03-14 23:31:02 -0300

    feat: funcao selection_sort criada

--- 3 - Algoritmos/selection_sort.py
+++ 3 - Algoritmos/selection_sort.py
@@ -1,3 +1,23 @@
 from gerar_lista import gerar_lista
 
 numeros = gerar_lista()
+
+
+def selection_sort(lista: list) -> list:
+    tamanho_lista = len(lista)
+
+    if tamanho_lista <= 0:
+        print('Lista vazia')
+        return []
+
+    else:
+        for i in range(tamanho_lista):
+            menor_indice = i
+            for j in range(i + 1, tamanho_lista):
+                if numeros[j] < numeros[i]:
+                    numeros[i], numeros[j] = numeros[j], numeros[i]
+
+    return numeros
+
+
+print(selection_sort(numeros))
